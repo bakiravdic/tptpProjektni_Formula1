c38fa96ba1e4d90130121eed31c00286152e4bb8
bakiravdic <bakir.avdic@fet.ba>
2026-05-10 02:57:32 +0200

    Update tptpskripte.js
    dodavanje logike za kalkulator bodova

--- Projektnitptp/js/tptpskripte.js
+++ Projektnitptp/js/tptpskripte.js
@@ -1,1 +1,10 @@
+function izracunaj() {
+    let bodovi = parseInt(document.getElementById("pozicija").value);
+    let dodatniBod = document.getElementById("najbrziKrug").checked;
 
+
+    if (dodatniBod && bodovi > 0) {
+        bodovi += 1;
+    }
+    document.getElementById("suma").innerText = bodovi;
+}
